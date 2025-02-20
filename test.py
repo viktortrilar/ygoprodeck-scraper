8947a1d4a59c88af186c4da34d8764a9e48e565a
Asterxxxx <viktor.trilar1@gmail.com>
2025-02-20 16:43:52 +0200

    add specific date filter and format output date to dd/mm/yyyy

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import requests
 import csv
 import os
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 KEY = os.getenv("SCRAPEAPI_KEY")
@@ -17,6 +18,7 @@ response_regionals = requests.get(scraper_url_regionals)
 response_YCS = requests.get(scraper_url_YCS)
 
 tournaments = []
+cutoff_date = datetime(2024, 12, 9)
 
 # Function to get tournament data
 def get_tournaments(response, tier_label):
@@ -24,8 +26,15 @@ def get_tournaments(response, tier_label):
         tournament_data = response.json() 
         if 'data' in tournament_data:            
             for tournament in tournament_data['data']:
+                # Check that tournament is the selected cutoff time then format date
+                og_date = tournament.get("event_date", "")
+                if og_date:
+                    tournament_date = datetime.strptime(og_date, "%Y-%m-%d")
+                    if tournament_date >= cutoff_date:
+                        format_date = tournament_date.strftime("%d-%m-%Y")
+
                 tournaments.append({
-                    "Date": tournament.get("event_date", ""),
+                    "Date": format_date,
                     "Country": tournament.get("country", ""),
                     "Tournament": tournament.get("name", ""),
                     "Participants": tournament.get("player_count", ""),
@@ -46,3 +55,5 @@ with open('tournament_data.csv', mode='w', newline='', encoding='utf-8') as file
     writer = csv.DictWriter(file, fieldnames=["Date", "Country", "Tournament", "Participants", "Winner", "Tier"])
     writer.writeheader()
     writer.writerows(tournaments)
+
+print("Saved to CSV")
